2c8e528ed184ebe4a952a611ce8c7c1d7338c3d6
Fabio Scaravelli <github@fabioscaravelli.com>
2024-06-21 15:10:50 -0300

    Update rss2telegram.py

--- rss2telegram.py
+++ rss2telegram.py
@@ -26,8 +26,6 @@ DRYRUN = os.environ.get('DRYRUN')
 TOPIC = os.environ.get('TOPIC', False)
 TELEGRAPH_TOKEN = os.environ.get('TELEGRAPH_TOKEN', False)
 
-print(BOT_TOKEN)
-print(os.environ.get('FEEDS')
 FEEDS = get_variable('FEEDS')
 feeds = json.loads(FEEDS)
 
